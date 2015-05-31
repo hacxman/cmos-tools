feb976618d08a7fb5a7e6630b725385e8145045f
Maros Zatko <hacxman@gmail.com>
2015-05-31 22:53:07 +0200

    factor out xinv, xbuf and xand

--- gen.py
+++ gen.py
@@ -8,26 +8,71 @@ produces PROM implementation from given binary data
 '''
 
 @contextmanager
-def new_subckt(name, arity):
+def new_subckt(name, arity, extra=None):
   # we start numbering of input nodes from 1, since zero is GND
-  print(".SUBCKT {} {} out VDD".format(name, ' '.join(map(str, range(1, arity + 1)))))
+  if extra is None:
+    extra = []
+  print(".SUBCKT {} {} VDD".format(name, ' '.join(map(str, range(1, arity + 1)) + extra)))
+
   yield
   end_subckt(name)
 
 def end_subckt(name):
-  print(".ENDS {}\n".format(name))
+  #print(".ENDS {}\n".format(name))
+  print(".ENDS")
 
 def gen_t_and(name, arity):
-  with new_subckt(name, arity):
+  with new_subckt(name, arity, ["out"]):
     print "* THIS WILL BE n-AND"
+    ox = 'VDD'
+    for x in xrange(1, arity):
+      #           d  g   s   b
+      print "MP{} {} {} {} {} PMOS".format(x, ox, x, 's'+str(x), 's'+str(x))
+      ox = "s{}".format(x)
+    x = arity
+    print "MP{} {} {} out out PMOS".format(x, ox, x)
+
+    ox = 'out'
+    for x in xrange(arity, 1, -1):
+      print "MN{} {} {} {} {} NMOS".format(x, ox, x, 'z'+str(x), 'z'+str(x))
+      ox = "z{}".format(x)
+    x = 1
+    print "MN{} {} {} 0 0 NMOS".format(x, ox, x)
+    print ".model nmos nmos level=54 version=4.6.5"
+    print ".model pmos pmos level=54 version=4.6.5"
+
 
 def gen_inv():
   with new_subckt('INV', 2):
     print "* THIS WILL BE CMOS INVERTOR"
+    print "MP vdd 1 2 2 PMOS"
+    print "MN   2 1 0 0 NMOS"
+    print ".model nmos nmos level=54 version=4.6.5"
+    print ".model pmos pmos level=54 version=4.6.5"
 
 def gen_buf():
   with new_subckt('BUF', 2):
     print "* THIS WILL BE CMOS BUFFER"
+    print "XINV1 1 3 VDD INV"
+    print "XINV2 3 2 VDD INV"
+
+xinvid=1
+def xinv(inp, outp):
+  global xinvid
+  print('XINV{} {} {} VDD INV'.format(xinvid, inp, outp))
+  xinvid = xinvid + 1
+
+xbufid=1
+def xbuf(inp, outp):
+  global xbufid
+  print('XBUF{} {} {} VDD BUF'.format(xbufid, inp, outp))
+  xbufid = xbufid + 1
+
+xandid=1
+def xand(input_arity, inps, outp):
+  global xandid
+  print('XAND{} {} {} VDD AND{}'.format(xandid, ' '.join(inps), outp, input_arity))
+  xandid = xandid + 1
 
 def gen_and_matcher(d, ident, and_name, bitlen, inps, outps):
   # input data, identifier, and model name, its input len in bits
@@ -39,13 +84,16 @@ def gen_and_matcher(d, ident, and_name, bitlen, inps, outps):
   for idx, (bit, inp) in enumerate(zip(bits, inps)):
     if bit == '0':
       #print 'XINV {}in{} {}inand{} VDD INV'.format(ident, idx, ident, idx)
-      print 'XINV {} {}inand{} VDD INV'.format(inp, ident, idx)
+      #print 'XINV{} {} {}inand{} VDD INV'.format(idx, inp, ident, idx)
+      xinv(inp, "{}inand{}".format(ident, idx))
     elif bit == '1':
       #print 'XBUF {}in{} {}inand{} VDD BUF'.format(ident, idx, ident, idx)
-      print 'XBUF {} {}inand{} VDD BUF'.format(inp, ident, idx)
-  print 'XAND {} {} VDD AND{}'.format(
-      ' '.join(map(lambda _x: ident+'inand'+str(_x), range(bitlen))),
-      outps[0], bitlen)
+      #print 'XBUF{} {} {}inand{} VDD BUF'.format(idx, inp, ident, idx)
+      xbuf(inp, "{}inand{}".format(ident, idx))
+#  print 'XAND {} {} VDD AND{}'.format(
+#      ' '.join(map(lambda _x: ident+'inand'+str(_x), range(bitlen))),
+#      outps[0], bitlen)
+  xand(bitlen, map(lambda _x: ident+'inand'+str(_x), range(bitlen)), outps[0])
 
   print '\n'
 
@@ -90,8 +138,10 @@ def main():
   foname = sys.argv[2]
   with open(finame) as fin:
     d = fin.read()
+    print "CMOS n-bit PROM"
     gen_ands(d)
     gen_ors(map(ord, d), 8) # data "byte" is 8 bits
+    print ".end"
     with open(foname, 'w') as fout:
       fout.write(d)
 
